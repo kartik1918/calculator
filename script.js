f00d75d5a4d33ea64d25a5210cf9bd46fb4c8042
Kartik Gupta <kartikgupta966@gmail.com>
2022-03-08 17:58:19 +0530

    html css done

--- script.js
+++ script.js
@@ -1,29 +1,53 @@
+const screen = document.querySelector('.screen');
+const btns = Array.from(document.querySelectorAll('button'));
+const divideBtn = document.getElementById('divide');
+const multiplyBtn = document.getElementById('multiply');
+const subtractBtn = document.getElementById('subtract');
+const equalsBtn = document.getElementById('equals');
+const addBtn = document.getElementById('add');
+const numBtns = document.querySelectorAll('.btn')
+let num;
+
+numBtns.forEach(function(numBtn) {
+    numBtn.addEventListener('click', function() {
+        num = Number(numBtn.textContent)
+        screen.innerHTML += `<p class="display-text">${Number(numBtn.textContent)}</p>`
+    })
+})
+
+addBtn.addEventListener('click', function(e) {
+    console.log(addBtn.textContent)
+    screen.innerHTML += `<p class="display-text">${addBtn.textContent}</p>`
+})
+
 function add(a, b) {
-    return a+b;
+    return a + b;
 }
 
 function subtract(a, b) {
-    return a-b;
-}
-
-function multiply(a, b) {
-    return a*b;
+    return a - b;
 }
 
 function divide(a, b) {
-    return a/b;
+    return a / b;
 }
 
-function operate(operator, num1, num2) {
-    switch (operator) {
+function multiply(a, b) {
+    return a * b;
+}
 
-        case '+' : return add(num1, num2);
-        
-        case '-' : return subtract(num1, num2);
-        
-        case '*' : return multiply(num1, num2);
-        
-        case '/' : return divide(num1, num2);
-                    
+function operate(a, b, operand) {
+    switch (operand) {
+        case '+': add(a, b);
+        break;
+
+        case '-': subtract(a, b);
+        break;
+
+        case '÷': divide(a, b);
+        break;
+
+        case 'x': multiply(a, b);
+        break;
     }
 }
